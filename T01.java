c28018b80bb4964329a339d56b64206d262d9279
Nicolas Butarbutar <nicolaszuliaaan@gmail.com>
2024-09-04 21:27:57 +0700

    Telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,20 @@
-// NIM - NAMA
+// 12S242038 - NICOLAS J GRACE BUTARBUTAR
 // NIM - NAMA
 
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+
+        nama = input.nextLine();
+        System.out.println("Nama saya adalah " + nama);
+        int bilangan;
+
+        bilangan = input.nextInt();
+        System.out.println("Tahun lahir saya adalah " + bilangan);
+    }
+}
